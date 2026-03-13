f735e56c9c577e032af1c407f03bb96830a3ffda
Maderfacar <129809010+Maderfacar@users.noreply.github.com>
2026-03-13 17:08:35 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+// config.js
+export const CONFIG = {
+  // 1. Firebase 配置 (以後換資料庫改這裡就好)
+  firebaseConfig: {
+    apiKey: "AIzaSyCg7ONT1mDIXZplNhTKPHw-cn5UbXFQJyk",
+    authDomain: "destination-anywhere-cfd50.firebaseapp.com",
+    projectId: "destination-anywhere-cfd50",
+    appId: "1:718691467645:web:fb37ce5fa5708c1f788c14"
+  },
+  // 2. 你的 UID
+  ADMIN_UID: "Uc5716677131e3d507708995b4e9f7b79",
+  // 3. LIFF ID
+  LIFF_ID: "2009434590-UHOSSHQI",
+  // 4. 共用樣式 (如果需要動態切換主題色)
+  theme: {
+    primary: "#0071E3",
+    danger: "#FF453A"
+  }
+};
