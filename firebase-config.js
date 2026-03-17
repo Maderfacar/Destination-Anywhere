ecc7b1ab7b98d62e1c3d50e7b6bf88cd27446896
Maderfacar <129809010+Maderfacar@users.noreply.github.com>
2026-03-18 01:21:27 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // firebase-config.js
 // 這裡是專案的核心設定，負責初始化並導出常用工具
-import { CONFIG } from './config.js?v=6';
+// 使用版本號 ?v=99 強制避開瀏覽器快取舊的 Provider ID
+import { CONFIG } from './config.js?v=99';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-check.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -10,7 +11,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-s
 const app = initializeApp(CONFIG.firebaseConfig);
 
 // 2. 啟動 App Check (防禦機制)
-// 這能確保只有你的 LIFF 頁面可以存取資料庫，防止惡意爬蟲
+// 注意：如果換了網域，記得去 Google reCAPTCHA 增加授權網域
 const appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider(CONFIG.appCheckSiteKey),
     isTokenAutoRefreshEnabled: true 
